62cb9c7a39b2e1a0807283090050415dc92c0481
Edward Tanguay (wk) <edward@tanguay.info>
2021-01-20 17:49:09 +0100

    first commit

---
+++ dev/test.js
@@ -0,0 +1,6 @@
+
+const port = process.env.PORT || 3000;
+const node_env = process.env.NODE_ENV || 'development';
+console.log(`port: ${port}, node_env: ${node_env}`);
+
+// console.log(process.env);
